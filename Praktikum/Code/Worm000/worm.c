5f30d442257497d687d7ce77a2a7296b1a6721c7
PascalSchwab <blazerclips@gmail.com>
2022-11-08 10:49:34 +0100

    Worm000 Format

--- Praktikum/Code/Worm000/worm.c
+++ Praktikum/Code/Worm000/worm.c
@@ -54,58 +54,58 @@ void cleanupCursesApp(void)
 // ********************************************************************************************
 
 int main(void) {
-    int res_code;         // Result code from functions
+    int res_code;   // Result code from functions
     char* message_template = "Das Fenster hat xxx Zeilen und yyy Spalten";
     int msg_len;
     int min_rows = 3;
 
     // Here we start
-    initializeCursesApplication();                             // Init various settings of our application
+    initializeCursesApplication();    // Init various settings of our application (01)
 
-    msg_len = strlen(message_template);                   // Compute length of our template
+    msg_len = strlen(message_template);   // Compute length of our template (02)
 
     // Maximal LINES and COLS are set by curses for the current window size.
     // Check if the window is large enough to display our message
-    if ( LINES < min_rows       || COLS < msg_len     ) {
+    if (LINES < min_rows || COLS < msg_len) {   // (03)
         // Cleanup special curses settings and restore the normal terminal functionality
-        cleanupCursesApp();
+        cleanupCursesApp();   // (04)
         // Print a conventional error message via printf.
         // Note: this only work after the call to  cleanupCursesApp();
-        printf("Das Fenster ist zu klein: wir brauchen mindestens %dx%d\n", msg_len, min_rows );
-
+        printf("Das Fenster ist zu klein: wir brauchen mindestens %dx%d\n", msg_len, min_rows);   // (05)
         // Set the result code to report the error
         res_code = RES_FAILED;
-    } else {
+    }
+    else{
         // Center output
         int mid_row = LINES/2;
-        int start_col = COLS/2-msg_len/2;
+        int start_col = COLS/2-msg_len/2;    // (06)
 
-        // Write letter A to the top    left  corner of our display
+        // Write letter A to the top left corner of our display
         move(0,0);
-        addch('A');
-        // Write letter B to the top    right corner of our display
-        // Use combination of move() and addch() functions
-        mvaddch(0, COLS-1, 'B');
+        addch('A');   // (07)
+        // Write letter B to the top right corner of our display
+        mvaddch(0, COLS-1, 'B');    // (08)
         // Write letter C to the bottom right corner of our display
-        mvaddch(LINES-1, COLS-1, 'C');
+        mvaddch(LINES-1, COLS-1, 'C');    // (09)
         // Write letter D to the bottom left  corner of our display
-        mvaddch(LINES-1, 0, 'D');
+        mvaddch(LINES-1, 0, 'D');   // (10)
  
         // Write our message centered onto the display
-        mvprintw(mid_row, start_col,"Das Fenster hat %3d Zeilen und %3d Spalten", LINES, COLS);
+        mvprintw(mid_row, start_col,"Das Fenster hat %3d Zeilen und %3d Spalten", LINES, COLS);   // (11)
 
         // Refresh the screen in order to show all changes on the screen
-        refresh();
+        refresh();    // (12)
         
         // Wait for user to press a key
-        nodelay(stdscr, FALSE);                   // make getch to be a blocking call
+        nodelay(stdscr, FALSE);   // Make getch to be a blocking call (13)
         getch();
 
         // Set the result code to report success
         res_code = RES_OK;
-
-        // Cleanup special curses settings and restore the normal terminal functionality
-        cleanupCursesApp();
     }
+    
+    // Cleanup special curses settings and restore the normal terminal functionality
+    cleanupCursesApp();   // (14)
+    
     return res_code;
 }
